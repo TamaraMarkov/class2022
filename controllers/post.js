f7013dda5522d2d2633189f048c27a370014f7e5
leah <leahbrodsky456@gmail.com>
2022-03-11 09:52:12 +0200

    initial commit

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,56 @@
+const Post = require('../models/post_model')
 /**
  * Gets all the posts
  * @param {http request} req 
  * @param {http response} res 
  */
 
-const getAllPosts = (req,res)=>{
-    res.send('app get post')
+
+const getAllPosts = async (req,res)=>{
+    console.log('getAllPosts')
+    
+    try {
+        const sender = req.query.sender
+        var posts
+        if (sender != null | sender != undefined) {
+            posts = await Post.find({'sender' : sender})    
+        }else{
+            posts = await Post.find()
+        }
+        res.status(200).send(posts)
+    } catch (err) {
+        res.status(400).send({
+            'message' : err.message
+        })
+   
+    }
 }
+
+const getPostById = async(req,res)=>{
+    console.log('getPostById id=' + req.params.id)
+    const id = req.params.id
+    if (id == null || id == undefined) 
+    {
+        res.status(400).send({
+            'status':'fail',
+            'message':err.message})     
+    }
+    try {
+        post = await Post.findById(id)
+        res.status(200).send(post)
+    } catch (err) {
+        res.status(400).send({
+            'status':'fail',
+            'message' : err.message
+        })
+    }
+
+}
+
+const deleteAllPosts = (req,res)=>{
+    res.send('app delete post')
+}
+
 
 /**
  * Create new post
@@ -14,12 +58,30 @@ const getAllPosts = (req,res)=>{
  * @param {http response} res 
  */
 
-const createNewPost = (req,res)=>{
-    res.send('app post post')
+const createNewPost = async (req,res)=>{
+    console.log(req.body)
+
+    const post = Post({
+        message: req.body.message,
+        sender: req.body.sender
+    })
+    try{
+        newPost = await post.save()
+        res.status(200).send(newPost)
+        
+
+    }catch(err){
+        res.status(400).send({
+            'message' : err.message
+        })
+    }
+    
 }
 
 
 module.exports = {
     getAllPosts,
-    createNewPost
+    createNewPost,
+    deleteAllPosts,
+    getPostById
 }
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -4,6 +4,11 @@ const Post = require('../controllers/post')
 
 router.get('/',Post.getAllPosts)
 
+router.get('/:id',Post.getPostById)
+
+
+router.delete('/',Post.deleteAllPosts)
+
 router.post('/',Post.createNewPost)
 
 module.exports = router
